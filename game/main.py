2dfe804cd8c78dcd9d32aa120410068b8e421eb0
jvbsilva <joaovictorbarros1@gmail.com>
2024-08-09 10:25:48 -0300

    Creating list of available points and food list

--- game/main.py
+++ game/main.py
@@ -1,9 +1,11 @@
+import random
 import pygame
 from pygame.surface import Surface
 import sys
 from snakes.Snake import Snake
 
 FPS = 60
+N_FOODS = 5
 
 SCREEN_WIDTH = 800
 SCRERN_HEIGHT = 600
@@ -23,6 +25,7 @@ LIGHT_GREY = (211, 211, 211)
 WHITE_SMOKE = (245, 245, 245)
 WHITE = (255, 255, 255)
 RED = (255, 55, 55)
+BLUE = (55, 55, 255)
 
 
 def draw_snake(screen: Surface, snake: Snake, color: tuple):
@@ -34,18 +37,34 @@ def draw_snake(screen: Surface, snake: Snake, color: tuple):
         )
 
 
+def use_available_point(available_points: list):
+    point = random.choice(available_points)
+    available_points.remove(point)
+    return list(point)
+
+
 def main():
+
+    # Create available_points list
+    available_points = GRID.copy()
+
+    # Create food_list
+    food_list = []
+    for _ in range(N_FOODS):
+        food_list.append(use_available_point(available_points))
+
+    # Create user snake
+    userSnake = Snake(use_available_point(available_points), GRID_SQUARE)
+
     # Initialize PyGame
     pygame.init()
 
-    # Create clock to control fps
-    clock = pygame.time.Clock()
     # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCRERN_HEIGHT))
     pygame.display.set_caption("PySnake")
 
-    # Create user snake
-    userSnake = Snake([300, 300], GRID_SQUARE)
+    # Create clock to control fps
+    clock = pygame.time.Clock()
 
     # Main loop
     while True:
@@ -70,7 +89,7 @@ def main():
         # Move user snake
         userSnake.move()
 
-        # Fill the screen with black color
+        # Clean the screen
         screen.fill(WHITE)
 
         # Draw the grid
@@ -78,6 +97,12 @@ def main():
         for x, y in GRID_WHITE:
             pygame.draw.rect(
                 screen, WHITE_SMOKE, pygame.Rect(x, y, GRID_SQUARE, GRID_SQUARE)
+            )
+
+        # Draw foods
+        for food in food_list:
+            pygame.draw.rect(
+                screen, BLUE, pygame.Rect(food[0], food[1], GRID_SQUARE, GRID_SQUARE)
             )
 
         # Draw user snake
