b3e6220be5d1c38824f1f2d35dcdabd3e562808b
jvbsilva <joaovictorbarros1@gmail.com>
2024-08-08 22:47:13 -0300

    Adding motion to user snake

--- game/main.py
+++ game/main.py
@@ -3,6 +3,8 @@ from pygame.surface import Surface
 import sys
 from snakes.Snake import Snake
 
+FPS = 60
+
 SCREEN_WIDTH = 800
 SCRERN_HEIGHT = 600
 
@@ -13,29 +15,37 @@ GRID_SQUARE = 10
 X_GRID = range(0, GRID_WIDTH, GRID_SQUARE)
 Y_GRID = range(0, GRID_HEIGHT, GRID_SQUARE)
 
-GRID =  [(x,y) for x in X_GRID for y in Y_GRID]
-GRID_WHITE = [(x,y) for x, y in GRID if ((x  + y) / GRID_SQUARE) % 2 == 0 ]
+GRID = [(x, y) for x in X_GRID for y in Y_GRID]
+GRID_WHITE = [(x, y) for x, y in GRID if ((x + y) / GRID_SQUARE) % 2 == 0]
 
-BLACK = (0,0,0)
-LIGHT_GREY = (211,211,211)
-WHITE_SMOKE = (245,245,245)
-WHITE = (255,255,255)
-RED = (255,55,55)
+BLACK = (0, 0, 0)
+LIGHT_GREY = (211, 211, 211)
+WHITE_SMOKE = (245, 245, 245)
+WHITE = (255, 255, 255)
+RED = (255, 55, 55)
+
 
 def draw_snake(screen: Surface, snake: Snake, color: tuple):
     for piece in snake.body:
-        pygame.draw.rect(screen, color, pygame.Rect(piece[0], piece[1], snake.square_size, snake.square_size))
+        pygame.draw.rect(
+            screen,
+            color,
+            pygame.Rect(piece[0], piece[1], snake.square_size, snake.square_size),
+        )
+
 
 def main():
     # Initialize PyGame
     pygame.init()
 
+    # Create clock to control fps
+    clock = pygame.time.Clock()
     # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCRERN_HEIGHT))
-    pygame.display.set_caption('PySnake')
+    pygame.display.set_caption("PySnake")
 
     # Create user snake
-    userSnake = Snake([300,300],GRID_SQUARE)
+    userSnake = Snake([300, 300], GRID_SQUARE)
 
     # Main loop
     while True:
@@ -46,16 +56,19 @@ def main():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    userSnake.move_left()
+                    userSnake.turn("LEFT")
 
                 if event.key == pygame.K_RIGHT:
-                    userSnake.move_right()
-            
+                    userSnake.turn("RIGHT")
+
                 if event.key == pygame.K_UP:
-                    userSnake.move_up()
+                    userSnake.turn("UP")
 
                 if event.key == pygame.K_DOWN:
-                    userSnake.move_down()
+                    userSnake.turn("DOWN")
+
+        # Move user snake
+        userSnake.move()
 
         # Fill the screen with black color
         screen.fill(WHITE)
@@ -63,7 +76,9 @@ def main():
         # Draw the grid
         pygame.draw.rect(screen, LIGHT_GREY, pygame.Rect(0, 0, GRID_WIDTH, GRID_HEIGHT))
         for x, y in GRID_WHITE:
-            pygame.draw.rect(screen, WHITE_SMOKE, pygame.Rect(x, y, GRID_SQUARE, GRID_SQUARE))
+            pygame.draw.rect(
+                screen, WHITE_SMOKE, pygame.Rect(x, y, GRID_SQUARE, GRID_SQUARE)
+            )
 
         # Draw user snake
         draw_snake(screen, userSnake, RED)
@@ -71,5 +86,9 @@ def main():
         # Update the display
         pygame.display.flip()
 
-if __name__ == '__main__':
+        # Use clock to control FPS
+        clock.tick(FPS)
+
+
+if __name__ == "__main__":
     main()
--- game/snakes/Snake.py
+++ game/snakes/Snake.py
@@ -1,20 +1,34 @@
-
-class Snake():
-    def __init__(self,head: list, square_size: int):
+class Snake:
+    def __init__(self, head: list, square_size: int):
         self.head = head
         self.square_size = square_size
         self.body = []
         self.body.append(head)
-        
+        self.cur_direction = ""
+
+    def turn(self, direction: str):
+        self.cur_direction = direction
+
     def move_right(self):
         self.head[0] += self.square_size
-        
+
     def move_left(self):
         self.head[0] -= self.square_size
-    
+
     def move_up(self):
         self.head[1] -= self.square_size
-    
+
     def move_down(self):
         self.head[1] += self.square_size
 
+    def move(self):
+        if self.cur_direction == "RIGHT":
+            self.move_right()
+        elif self.cur_direction == "LEFT":
+            self.move_left()
+        elif self.cur_direction == "UP":
+            self.move_up()
+        elif self.cur_direction == "DOWN":
+            self.move_down()
+        else:
+            pass
